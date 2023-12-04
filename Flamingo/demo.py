a8c94c679585359a93b5e94d799fc907f1f423e3
YatesZhang <2230417198@qq.com>
2023-12-04 18:51:18 +0800

    doc.txt + demo

---
+++ Flamingo/demo.py
@@ -0,0 +1,18 @@
+# grab model checkpoint from huggingface hub
+from huggingface_hub import hf_hub_download
+import torch
+from open_flamingo import create_model_and_transforms
+
+cache_dir = "/home/yunzhi/yunzhi/yunzhi/checkpoints/flamingo"
+model, image_processor, tokenizer = create_model_and_transforms(
+    clip_vision_encoder_path="ViT-L-14",
+    clip_vision_encoder_pretrained="openai",
+    lang_encoder_path="anas-awadalla/mpt-1b-redpajama-200b",
+    tokenizer_path="anas-awadalla/mpt-1b-redpajama-200b",
+    cross_attn_every_n_layers=1,
+    cache_dir= cache_dir # Defaults to ~/.cache
+)
+
+checkpoint_path = hf_hub_download("openflamingo/OpenFlamingo-3B-vitl-mpt1b", "checkpoint.pt", cache_dir=cache_dir)
+# checkpoint_path = "/home/yunzhi/yunzhi/yunzhi/checkpoints/flamingo/checkpoint.pt"
+model.load_state_dict(torch.load(checkpoint_path), strict=False)
