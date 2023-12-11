6c4c20f3622b9d46e519d52ad1b96aa4da74b258
YatesZhang <2230417198@qq.com>
2023-12-11 11:10:40 +0800

    add lora tuning

--- Flamingo/demo.py
+++ Flamingo/demo.py
@@ -15,9 +15,89 @@ model, image_processor, tokenizer = create_model_and_transforms(
     lang_encoder_path="anas-awadalla/mpt-1b-redpajama-200b",
     tokenizer_path="anas-awadalla/mpt-1b-redpajama-200b",
     cross_attn_every_n_layers=1,
-    cache_dir= cache_dir # Defaults to ~/.cache
+    cache_dir= cache_dir,  # Defaults to ~/.cache
+    lora_tuning=True
 )
 
-checkpoint_path = hf_hub_download("openflamingo/OpenFlamingo-3B-vitl-mpt1b", "checkpoint.pt", cache_dir=cache_dir)
+# checkpoint_path = hf_hub_download("openflamingo/OpenFlamingo-3B-vitl-mpt1b", "checkpoint.pt", cache_dir=cache_dir)
 # checkpoint_path = "/home/yunzhi/yunzhi/yunzhi/checkpoints/flamingo/checkpoint.pt"
-model.load_state_dict(torch.load(checkpoint_path), strict=False)
+# model.load_state_dict(torch.load(checkpoint_path), strict=False)
+
+
+"""
+Step 1: Load images
+"""
+# demo_image_one = Image.open(
+#     requests.get(
+#         "http://images.cocodataset.org/val2017/000000039769.jpg", stream=True
+#     ).raw
+# )
+
+# demo_image_two = Image.open(
+#     requests.get(
+#         "http://images.cocodataset.org/test-stuff2017/000000028137.jpg",
+#         stream=True
+#     ).raw
+# )
+
+# query_image = Image.open(
+#     requests.get(
+#         "http://images.cocodataset.org/test-stuff2017/000000028352.jpg", 
+#         stream=True
+#     ).raw
+# )
+demo_image_two = Image.open("./images/2.jpg")
+demo_image_one = Image.open("./images/1.jpg")
+# demo_image_one = Image.open("./images/night.jpg")
+# query_image = Image.open("./images/snow.jpg")
+# query_image = Image.open("./images/night.jpg")
+query_image = Image.open("./images/yellow_bus.jpg")
+def to_cuda(data, device=0):
+    if isinstance(data, BatchEncoding):
+        # k: ['input_ids', 'attention_mask']
+        for k in data:
+            data[k] = data[k].cuda(device)
+    return data 
+
+if __name__ == "__main__":
+    device = 1
+    model = model.cuda(device)
+
+    """
+    Step 2: Preprocessing images
+    Details: For OpenFlamingo, we expect the image to be a torch tensor of shape 
+    batch_size x num_media x num_frames x channels x height x width. 
+    In this case batch_size = 1, num_media = 3, num_frames = 1,
+    channels = 3, height = 224, width = 224.
+    """
+    vision_x = [image_processor(demo_image_one).unsqueeze(0),
+        image_processor(demo_image_two).unsqueeze(0),
+        image_processor(query_image).unsqueeze(0)]
+    vision_x = torch.cat(vision_x, dim=0).cuda(device)
+    vision_x = vision_x.unsqueeze(1).unsqueeze(0)
+    print(vision_x.shape)
+
+    """
+    Step 3: Preprocessing text
+    Details: In the text we expect an <image> special token to indicate where an image is.
+    We also expect an <|endofchunk|> special token to indicate the end of the text 
+    portion associated with an image.
+    """
+    tokenizer.padding_side = "left" # For generation padding tokens should be on the left
+    lang_x = tokenizer(
+        ["<image>An image of two cats.<|endofchunk|><image>An image of a bathroom.<|endofchunk|><image>the color is"],
+        return_tensors="pt",
+    )
+    lang_x = to_cuda(lang_x, device=device)
+
+    """
+    Step 4: Generate text
+    """
+    generated_text = model.generate(
+        vision_x=vision_x,
+        lang_x=lang_x["input_ids"],
+        attention_mask=lang_x["attention_mask"],
+        max_new_tokens=20,
+        num_beams=3,
+    )
+    print("Generated text: ", tokenizer.decode(generated_text[0]))
--- Flamingo/lora_tuning.py
+++ Flamingo/lora_tuning.py
@@ -1,5 +1,5 @@
 from typing import Optional
-
+import torch 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import open_clip
 
@@ -7,7 +7,7 @@ from open_flamingo import Flamingo
 from open_flamingo.src.flamingo_lm import FlamingoLMMixin
 from open_flamingo.src.utils import extend_instance
 from bigmodelvis import Visualization
-
+from huggingface_hub import hf_hub_download
 import pdb 
 
 def create_model_and_transforms(
@@ -20,8 +20,28 @@ def create_model_and_transforms(
     decoder_layers_attr_name: str = None,
     freeze_lm_embeddings: bool = False,
     cache_dir: Optional[str] = None,
+    lora_tuning=False,
     **flamingo_kwargs,
 ):
+    """
+    Initialize a Flamingo model from a pretrained vision encoder and language encoder.
+    Appends special tokens to the tokenizer and freezes backbones.
+    
+    Args:
+        clip_vision_encoder_path (str): path to pretrained clip model (e.g. "ViT-B-32")
+        clip_vision_encoder_pretrained (str): name of pretraining dataset for clip model (e.g. "laion2b_s32b_b79k")
+        lang_encoder_path (str): path to pretrained language encoder
+        tokenizer_path (str): path to pretrained tokenizer
+        cross_attn_every_n_layers (int, optional): determines how often to add a cross-attention layer. Defaults to 1.
+        use_local_files (bool, optional): whether to use local files. Defaults to False.
+        decoder_layers_attr_name (str, optional): name of the decoder layers attribute. Defaults to None.
+        freeze_lm_embeddings (bool, optional): whether to freeze LM input embeddings when configuring Perceiver.
+        cache_dir (str, optional): path to cache directory for downloading OpenClip/HF weights.
+    Returns:
+        Flamingo: Flamingo model from pretrained vision and language encoders
+        Image processor: Pipeline to preprocess input images
+        Tokenizer: A tokenizer for the language model
+    """
     """
     Initialize a Flamingo model from a pretrained vision encoder and language encoder.
     Appends special tokens to the tokenizer and freezes backbones.
@@ -41,14 +61,15 @@ def create_model_and_transforms(
         Image processor: Pipeline to preprocess input images
         Tokenizer: A tokenizer for the language model
     """
+    print("[create_model_and_transforms] create vision_encoder and image_processor from open_clip")
     vision_encoder, _, image_processor = open_clip.create_model_and_transforms(
-        clip_vision_encoder_path,
-        pretrained=clip_vision_encoder_pretrained,
+        clip_vision_encoder_path,    # "ViT-L-14"
+        pretrained=clip_vision_encoder_pretrained,    # "openai"
         cache_dir=cache_dir,
     )
     # set the vision encoder to output the visual features
     vision_encoder.visual.output_tokens = True
-
+    print("[create_model_and_transforms] create text_tokenizer")
     text_tokenizer = AutoTokenizer.from_pretrained(
         tokenizer_path,
         local_files_only=use_local_files,
@@ -64,7 +85,7 @@ def create_model_and_transforms(
         # modify labels for the loss.
         text_tokenizer.add_special_tokens({"pad_token": "<PAD>"})
 
-
+    print("[create_model_and_transforms] create LLM from ", lang_encoder_path)
     lang_encoder = AutoModelForCausalLM.from_pretrained(
         lang_encoder_path,
         local_files_only=use_local_files,
@@ -74,7 +95,6 @@ def create_model_and_transforms(
 
     # hacks for MPT-1B, which doesn't have a get_input_embeddings method
     if "mpt-1b-redpajama-200b" in lang_encoder_path:
-
         class EmbeddingFnMixin:
             def get_input_embeddings(self):
                 return self.transformer.wte
@@ -92,6 +112,7 @@ def create_model_and_transforms(
     lang_encoder.set_decoder_layers_attr_name(decoder_layers_attr_name)
     lang_encoder.resize_token_embeddings(len(text_tokenizer))
 
+    print("[create_model_and_transforms] create Flamingo with cross_attn_every_n_layers=", cross_attn_every_n_layers)
     model = Flamingo(
         vision_encoder,
         lang_encoder,
@@ -104,9 +125,25 @@ def create_model_and_transforms(
         **flamingo_kwargs,
     )
 
+    # load checkpoint:
+    print("[create_model_and_transforms] load checkpoint.pt from huggingface ")
+    checkpoint_path = hf_hub_download("openflamingo/OpenFlamingo-3B-vitl-mpt1b",
+     "checkpoint.pt",
+      cache_dir=cache_dir)
+    # checkpoint_path = "/home/yunzhi/yunzhi/yunzhi/checkpoints/flamingo/checkpoint.pt"
+    model.load_state_dict(torch.load(checkpoint_path), strict=False)
+    
+    print("[create_model_and_transforms] Freeze all parameters ")
+    # Freeze all parameters
+    model.requires_grad_(False)
+    assert sum(p.numel() for p in model.parameters() if p.requires_grad) == 0
+
     # --------------------------------------------------------------------------
     from peft import LoraConfig, get_peft_model
-    lora_target_modules=["Wqkv", "out_proj", 
+    
+
+    lora_target_modules=["Wqkv",
+    #  "out_proj", 
                             "to_q", "to_kv", "to_out",
                             "ff.1", "ff.3"]
     # Lora for LLaMa:
@@ -126,19 +163,21 @@ def create_model_and_transforms(
         **tuning_config
     )
     # pdb.set_trace()
-    model = get_peft_model(model, peft_config=tuning_config)
+    print("[create_model_and_transforms] LoRa tuning mode: ", lora_tuning)
+    if lora_tuning:
+        print("[create_model_and_transforms] LoRa tuning adaptor injection: ", lora_target_modules)
+        model = get_peft_model(model, peft_config=tuning_config)
+        model.print_trainable_parameters()
     # vis model: 
     Visualization(lang_encoder).structure_graph()
-    model.print_trainable_parameters()
+    
     # --------------------------------------------------------------------------
 
-    # Freeze all parameters
-    model.requires_grad_(False)
-    assert sum(p.numel() for p in model.parameters() if p.requires_grad) == 0
+
 
     # Unfreeze perceiver, gated_cross_attn_layers, and LM input embeddings
-    model.perceiver.requires_grad_(True)
-    model.lang_encoder.gated_cross_attn_layers.requires_grad_(True)
+    # model.perceiver.requires_grad_(True)
+    # model.lang_encoder.gated_cross_attn_layers.requires_grad_(True)
     if not freeze_lm_embeddings:
         model.lang_encoder.get_input_embeddings().requires_grad_(True)
         # TODO: investigate also training the output embeddings when untied
