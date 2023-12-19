b9fab1b9e497326ff0f54ec277436e9a8b09d678
root <root@10.138.117.89>
2023-12-19 18:46:03 +0800

    training bash script for deepspeed

---
+++ tools/train_flamingo.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+deepspeed --include=localhost:2,3,4,5 train_flamingo.py \
+    --per_device_train_batch_size 2 \
+    --learning_rate 1e-5 \
+    --learning_rate_pretraining_components 0 \
+    --weight_decay 0 \
+    --gradient_accumulation_steps 1 \
+    --lr_scheduler_type cosine \
+    --num_warmup_steps 100 \
+    --seed 1234 \
+    --local_rank -1 \
+    --gradient_checkpointing \
+    --zero_stage 2 \
+    --precision bf16 \
+    --work_dir ../work_dir \
+    # --enable_tensorboard
