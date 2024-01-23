fffc7bc9f9822d688227413cff99ea2696162f44
root <root@10.138.117.7>
2024-01-23 14:49:34 +0800

    inference demo on participants property

--- Flamingo/inference/post_process.py
+++ Flamingo/inference/post_process.py
@@ -1,13 +1,18 @@
 import re 
+
+
 def remove_special_characters(text):
     # 匹配 ASCII 特殊字符的正则表达式
     pattern = r'[^\x00-\x7f]+'
     
     # 使用空字符串替换匹配到的特殊字符
     result = re.sub(pattern, ' ', text)
-    
     return result
+
 def post_process(texts, cats=None):
+    """ 
+        post process for GTSRB dataset
+    """
     result = []
     for text in texts:
         skip_flag = False
